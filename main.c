135a18a018ce608c98c02bf374f30d9fb6164f0f
Cientistex <christiansardinha@gmail.com>
2026-05-26 19:39:59 -0300

    adiciona estrutura básica do programa com variáveis e controle de fluxo

--- main.c
+++ main.c
@@ -3,7 +3,39 @@
 int main(){
     printf ("olá mundo\n");
     
-    int x
+    int x;
+    int y;
+    int z[10];
+    int *p;
+
+    if  (1) {
+        x = 10;
+        y = 20;
+        p = z;
+    }
+    else    {
+        x = 30;
+        y = 40;
+        p = z + 5;
+    }
+    for (int i = 0; i < 10; i++) {
+        p[i] = i;
+    }   
+    while (x < y) {
+        printf("%d ", p[x]);
+        x++;
+    }
+    switch (y) {
+        case 20:
+            printf("\nValor de y é 20\n");
+            break;
+        case 40:
+            printf("\nValor de y é 40\n");
+            break;
+        default:
+            printf("\nValor de y é desconhecido\n");
+    }
+
     
     return 0;
 }
