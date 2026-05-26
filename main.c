cab99eacaf8c1105088702095c515f6c3da0a5fd
Cientistex <christiansardinha@gmail.com>
2026-05-26 19:29:07 -0300

    adiciona loop para exibir valores de i e valor de y através de ponteiro

--- main.c
+++ main.c
@@ -5,5 +5,15 @@ int main() {
     int y=0;
     scanf("%d", &y);
     printf("o valor de y é: %d\n", y);
+
+    for (int i = 0; i < 10; i++) {
+        printf("i: %d\n", i);
+
+    }
+
+    int *p = NULL;
+    p = &y;
+    printf("o valor de y através do ponteiro é: %d\n", *p);
+    
     return 0;
 }
